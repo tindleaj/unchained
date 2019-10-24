6159acb574ba5493afc0c7220128d2668eca29e4
Austin Tindle <tindleaj@gmail.com>
2019-10-23 23:09:13 -0600

    Add api

---
+++ api/discord.js
@@ -0,0 +1,26 @@
+const https = require('https')
+
+let discordUrl = process.env.DISCORD_URL || 'https://discordapp.com/invite/godsunchained'
+console.log(process.env)
+
+module.exports = async (req, res) => {
+  const memberCount = await getDiscordMemberCount()
+  res.status(200).json({
+    memberCount
+  })
+}
+
+function getDiscordMemberCount() {
+  let expression = /\| (.*) members/
+
+  return new Promise(function(resolve, reject) {
+    https
+      .get(discordUrl, res => {
+        let data = ''
+
+        res.on('data', chunk => (data += chunk))
+        res.on('end', () => resolve(data.match(expression)[1]))
+      })
+      .on('error', err => reject(err))
+  })
+}
