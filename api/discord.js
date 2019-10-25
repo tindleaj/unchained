95abaa11e0daf656f2ef7e99676bbf21d407f274
Austin Tindle <tindleaj@gmail.com>
2019-10-24 22:44:33 -0600

    Switch https for axios

--- api/discord.js
+++ api/discord.js
@@ -1,31 +1,44 @@
 const https = require('https')
+const axios = require('axios')
+const modelMap = require('./_models.json')
 
 let discordUrl =
   process.env.discord_url || 'https://discordapp.com/api/v6/invites/godsunchained?with_counts=true'
+let myjsonUrl = process.env.myjson_url || 'https://api.myjson.com/'
 
 module.exports = async (req, res) => {
-  const json = await getDiscordInviteData()
-  const data = JSON.parse(json)
+  const data = await buildResponse()
+  res.status(200).json(data)
+}
 
-  res.status(200).json({
+async function buildResponse() {
+  const data = await getGuildInfo()
+
+  return {
     total_members: data.approximate_member_count,
     online_members: data.approximate_presence_count
-  })
+  }
 }
 
-function getDiscordInviteData() {
-  return new Promise(function(resolve, reject) {
-    https
-      .get(discordUrl, res => {
-        let data = ''
-
-        res.on('data', chunk => (data += chunk))
-        res.on('end', () => {
-          resolve(data)
-        })
-      })
-      .on('error', err => reject(err))
-  })
+function addSnapshot(snapshot) {
+  axios.post()
 }
 
-// getDiscordInviteData().then(res => console.log(JSON.parse(res)))
+function getSnapshots() {
+  let discordModelId = modelMap.discord_stats
+
+  return axios
+    .get(`${myjsonUrl}/bins/${discordModelId}`)
+    .then(response => response.data)
+    .catch(error => console.error(error))
+}
+
+function getGuildInfo() {
+  return axios
+    .get(discordUrl)
+    .then(response => response.data)
+    .catch(error => console.error(error))
+}
+
+// getGuildInfo().then(res => console.log(res))
+// buildResponse().then(res => console.log(res))
